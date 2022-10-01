b0cc310c0ea7b7a9cf970c25a575794ab9c3c85c
James Vaughan <James.Vaughan@utoronto.ca>
2022-09-30 20:58:33 -0400

    Added a new project for downloading a month's worth of data off AWS. (#2)

---
+++ GatherAWSData/AWS.cs
@@ -0,0 +1,63 @@
+﻿using Amazon.S3;
+using System.Threading.Channels;
+
+namespace GatherAWSData;
+
+static internal class AWS
+{
+    public static async Task<string?[]> DownloadData(AWSConfig config, string destinationDirectory, string areaName, int year, int month, int date)
+    {
+        if(!Directory.Exists(destinationDirectory))
+        {
+            Directory.CreateDirectory(destinationDirectory);
+        }
+        var awsNames = CreateAWSDataSourceNames(areaName, year, month, date);
+        string?[] fileNames = CreateFileNames(destinationDirectory, areaName, year, month, date);
+        var cancel = new CancellationToken();
+        using (var client = new AmazonS3Client(config.AwsKey, config.AwsSecret, Amazon.RegionEndpoint.USWest2))
+        {
+            for(int i = 0; i < awsNames.Length; i++)
+            {
+                try
+                {
+                    var response = await client.GetObjectAsync(config.BucketName, awsNames[i]);
+                    await response.WriteResponseStreamToFileAsync(fileNames[i], false, cancel);
+                }
+                catch
+                {
+                    fileNames[i] = null;
+                }
+            }
+        }
+        return fileNames;
+    }
+
+    private static string[] CreateAWSDataSourceNames(string areaName, int year, int month, int date)
+    {
+        return new string[]
+        {
+            $"norm_data/{areaName}/{year}/{month:00}/{date:00}/lotadata_{areaName}_{year}_{month:00}_{date:00}_00000.gz",
+            $"norm_data/{areaName}/{year}/{month:00}/{date:00}/lotadata_{areaName}_{year}_{month:00}_{date:00}_00001.gz",
+            $"norm_data/{areaName}/{year}/{month:00}/{date:00}/lotadata_{areaName}_{year}_{month:00}_{date:00}_00002.gz",
+            $"norm_data/{areaName}/{year}/{month:00}/{date:00}/lotadata_{areaName}_{year}_{month:00}_{date:00}_00003.gz",
+            $"norm_data/{areaName}/{year}/{month:00}/{date:00}/lotadata_{areaName}_{year}_{month:00}_{date:00}_00004.gz",
+            $"norm_data/{areaName}/{year}/{month:00}/{date:00}/lotadata_{areaName}_{year}_{month:00}_{date:00}_10000.gz",
+            $"norm_data/{areaName}/{year}/{month:00}/{date:00}/lotadata_{areaName}_{year}_{month:00}_{date:00}_10001.gz",
+        };
+    }
+
+    private static string[] CreateFileNames(string directory, string areaName, int year, int month, int date)
+    {
+        return new string[]
+        {
+
+            Path.Combine(directory, $"lotadata_{areaName}_{year}_{month:00}_{date:00}_00000.gz"),
+            Path.Combine(directory, $"lotadata_{areaName}_{year}_{month:00}_{date:00}_00001.gz"),
+            Path.Combine(directory, $"lotadata_{areaName}_{year}_{month:00}_{date:00}_00002.gz"),
+            Path.Combine(directory, $"lotadata_{areaName}_{year}_{month:00}_{date:00}_00003.gz"),
+            Path.Combine(directory, $"lotadata_{areaName}_{year}_{month:00}_{date:00}_00004.gz"),
+            Path.Combine(directory, $"lotadata_{areaName}_{year}_{month:00}_{date:00}_10000.gz"),
+            Path.Combine(directory, $"lotadata_{areaName}_{year}_{month:00}_{date:00}_10001.gz"),
+        };
+    }
+}
---
+++ GatherAWSData/Entry.cs
@@ -0,0 +1,29 @@
+﻿namespace GatherAWSData;
+
+internal sealed record class Entry
+    (string Lat, string Long, string TS, string HAccuracy)
+{
+    // Original Structure: 'did','idtype','lat','lon','ts','haccuracy','device_brand','device_model','telco','ip','country','src','h10'
+
+    // Target Structure: 'did','lat','lon','ts','haccuracy'
+
+    public static Dictionary<string, List<Entry>> LoadEntries(string filePath)
+    {
+        var ret = new Dictionary<string, List<Entry>>();
+        foreach (var parts in File.ReadLines(filePath).Skip(1).Select(l => l.Split(',')))
+        {
+            if (parts.Length < 13)
+            {
+                continue;
+            }
+            var id = parts[0];
+            var entry = new Entry(parts[2], parts[3], parts[4], parts[5]);
+            if (!ret.TryGetValue(id, out var list))
+            {
+                ret[id] = list = new List<Entry>();
+            }
+            list.Add(entry);
+        }
+        return ret;
+    }
+}
